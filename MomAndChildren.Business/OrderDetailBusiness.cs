7bc02633bd5b98df25e3f6313d2f0339e7e207c0
Son-2003 <nguyenducson2915@gmail.com>
2024-05-20 00:35:27 +0700

    feat(OrderDetail): View, Create OrderDetail

---
+++ MomAndChildren.Business/OrderDetailBusiness.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MomAndChildren.Data.Models;
+using MomAndChildren.Data.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndChildren.Business
+{
+    public interface IOrderDetailBusiness
+    {
+        Task<IMomAndChildrenResult> GetOrderDetailsAsync();
+        Task<IMomAndChildrenResult> GetOrderDetailByIdAsync(int orderDetailId);
+        Task<IMomAndChildrenResult> CreateOrderDetail(int orderId, List<ProductCart> products);
+    }
+
+    public class OrderDetailBusiness : IOrderDetailBusiness
+    {
+        private readonly Net1710_221_3_MomAndChildrenContext _context;
+
+        public OrderDetailBusiness(Net1710_221_3_MomAndChildrenContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IMomAndChildrenResult> CreateOrderDetail(int orderId, List<ProductCart> products)
+        {
+            //create new order details
+            var orderDetails = new List<OrderDetail>();
+            foreach (var item in products)
+            {
+                double price = item.Product.Price;
+                int quantityCart = item.QuantityCart;
+
+                var orderDetail = new OrderDetail
+                {
+                    OrderId = orderId,
+                    UnitPrice = price,
+                    TotalPrice = price * quantityCart,
+                    ProductId = item.Product.ProductId,
+                    Quantity = quantityCart,
+                };
+                orderDetails.Add(orderDetail);
+            }
+            await _context.OrderDetails.AddRangeAsync(orderDetails);
+            await _context.SaveChangesAsync();
+            return new MomAndChildrenResult(1, "Create OrderDetails Successfully!");
+        }
+
+        public Task<IMomAndChildrenResult> GetOrderDetailByIdAsync(int orderDetailId)
+        {
+            OrderDetail? orderDetail = _context.OrderDetails.Find(orderDetailId);
+            if (orderDetail == null)
+            {
+                return Task.FromResult<IMomAndChildrenResult>(new MomAndChildrenResult(-1, "OrderDetail not found"));
+            }
+            else
+            {
+                return Task.FromResult<IMomAndChildrenResult>(new MomAndChildrenResult(1, "Get OrderDetail success", orderDetail));
+            }
+        }
+
+        public Task<IMomAndChildrenResult> GetOrderDetailsAsync()
+        {
+            IEnumerable<OrderDetail> orderDetails = _context.OrderDetails.OrderByDescending(x => x.OrderDetailId);
+            return Task.FromResult<IMomAndChildrenResult>(new MomAndChildrenResult(1, "Get OrderDetails success", orderDetails));
+        }
+    }
+}
--- MomAndChildren.Business/obj/Debug/net7.0/MomAndChildren.Business.AssemblyInfo.cs
+++ MomAndChildren.Business/obj/Debug/net7.0/MomAndChildren.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomAndChildren.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294387426ce9aa653cdd6cb4e6a533531f4a91da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomAndChildren.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomAndChildren.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
