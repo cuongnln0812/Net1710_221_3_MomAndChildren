bbd09f64723722a5a495a51bd7dd4c8117f29427
Son-2003 <nguyenducson2915@gmail.com>
2024-05-25 16:19:55 +0700

    readd files

---
+++ MomAndChildren.Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using MomAndChildren.Data.Models;
+using MomAndChildren.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndChildren.Data
+{
+    public class UnitOfWork
+    {
+        private Net1710_221_3_MomAndChildrenContext _unitOfWorkContext;
+
+        private OrderDetailRepository _orderDetail;
+
+        public UnitOfWork() { }
+
+        public OrderDetailRepository OrderDetailRepository
+        { 
+            get { 
+                return _orderDetail ??= new Repositories.OrderDetailRepository(); 
+            } 
+        }
+    }
+}
