c66e394fdd6e2f4f2aa3cb07146c5a9558811461
namlee1805 <hoainamheo2003@gmail.com>
2024-05-25 16:37:01 +0700

    fix

---
+++ MomAndChildren.Business/MomAndChildrenResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndChildren.Business
+{
+
+    public interface IMomAndChildrenResult
+    {
+        int Status { get; set; }
+        string? Message { get; set; }
+        object? Data { get; set; }
+    }
+
+    public class MomAndChildrenResult : IMomAndChildrenResult
+    {
+        public int Status { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+
+        public MomAndChildrenResult()
+        {
+            Status = -1;
+            Message = "Action fail";
+        }
+
+        public MomAndChildrenResult(int status, string message)
+        {
+            Status = status;
+            Message = message;
+        }
+
+        public MomAndChildrenResult(int status, string message, object data)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+
+
+    }
+}
---
+++ MomAndChildren.Data/Repository/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MomAndChildren.Data.Base;
+using MomAndChildren.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomAndChildren.Data.Repository
+{
+    public class OrderRepository : GenericRepository<Order>
+    {
+        public OrderRepository() 
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(int customerId)
+        {
+            return await _context.Orders.Where(o => o.CustomerId == customerId).ToListAsync();
+        }
+    }
+}
