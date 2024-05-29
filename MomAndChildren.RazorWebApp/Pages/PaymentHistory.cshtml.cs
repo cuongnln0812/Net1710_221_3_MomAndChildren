bc4fbd985990ae9dfed79ec5069ba6fae610b82e
henrynguyen1203@gmail.com <henrynguyen1203@gmail.com>
2024-05-29 19:29:44 +0700

    fix

--- MomAndChildren.RazorWebApp/Pages/PaymentHistory.cshtml.cs
+++ MomAndChildren.RazorWebApp/Pages/PaymentHistory.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MomAndChildren.Business;
+using MomAndChildren.Data.Models;
 
 namespace MomAndChildren.RazorWebApp.Pages
 {
@@ -14,11 +15,55 @@ namespace MomAndChildren.RazorWebApp.Pages
             _paymentHistoryBusiness = paymentHistoryBusiness;
         }
 
-        public IMomAndChildrenResult result;
 
-        public async Task OnGetAsync()
+        [BindProperty]
+        public Order Order { get; set; }
+        public List<PaymentHistory> PaymentHistories {get; set;}
+        public List<Order> SelectiveOrderList { get; set; }
+
+        [BindProperty]
+        public string PaymentMethod { get; set; }
+        public string Message { get; set; }
+
+        public void OnGet()
         {
-            result = await _paymentHistoryBusiness.GetPaymentHistoryList();
+            PaymentHistories = this.GetPaymentHistories();
         }
-    }
+
+        public void OnPost()
+        {
+            this.CreatePayment();
+        }
+
+        private List<Order> GetOrders()
+        {
+
+        }
+
+        private List<PaymentHistory> GetPaymentHistories()
+        {
+            var result = _paymentHistoryBusiness.GetPaymentHistoryList();
+
+            if (result.Status > 0 && result.Result.Data != null)
+            {
+                var paymentHistories = (List<PaymentHistory>)result.Result.Data;
+                return paymentHistories;
+            }
+            return new List<PaymentHistory>();
+        }
+
+        private void CreatePayment()
+        {
+            var result = _paymentHistoryBusiness.CreatePaymentHistory(Order, PaymentMethod);
+
+            if (result != null)
+            {
+                Message = result.Result.Message;
+            }
+            else
+            {
+                this.Message = "Error system";
+            }
+
+        }
 }
